f4d0ee2a011a8b27d2269b3825be32155ff8cf6d
Oleg Utkin <oleg.utkin@uber.com>
2019-06-20 17:06:44 -0700

    Rename ShrotenPath function

--- pure.zsh
+++ pure.zsh
@@ -23,98 +23,103 @@
 # \e[K  => clears everything after the cursor on the current line
 # \e[2K => clear everything on the current line
 
-shrink_path () {
-        setopt localoptions
-        setopt rc_quotes null_glob
+prompt_pure_shrink_path () {
+  # src: https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/shrink-path
 
-        typeset -i lastfull=0
-        typeset -i short=0
-        typeset -i tilde=0
-        typeset -i named=0
+  setopt localoptions
+  setopt rc_quotes null_glob
 
-        if zstyle -t ':prompt:shrink_path' fish; then
-                lastfull=1
-                short=1
-                tilde=1
-        fi
-        if zstyle -t ':prompt:shrink_path' nameddirs; then
-                tilde=1
-                named=1
-        fi
-        zstyle -t ':prompt:shrink_path' last && lastfull=1
-        zstyle -t ':prompt:shrink_path' short && short=1
-        zstyle -t ':prompt:shrink_path' tilde && tilde=1
+  typeset -i lastfull=0
+  typeset -i short=0
+  typeset -i tilde=0
+  typeset -i named=0
 
-        while [[ $1 == -* ]]; do
-                case $1 in
-                        -f|--fish)
-                                lastfull=1
-                                short=1
-                                tilde=1
-                        ;;
-                        -h|--help)
-                                print 'Usage: shrink_path [-f -l -s -t] [directory]'
-                                print ' -f, --fish      fish-simulation, like -l -s -t'
-                                print ' -l, --last      Print the last directory''s full name'
-                                print ' -s, --short     Truncate directory names to the first character'
-                                print ' -t, --tilde     Substitute ~ for the home directory'
-                                print ' -T, --nameddirs Substitute named directories as well'
-                                print 'The long options can also be set via zstyle, like'
-                                print '  zstyle :prompt:shrink_path fish yes'
-                                return 0
-                        ;;
-                        -l|--last) lastfull=1 ;;
-                        -s|--short) short=1 ;;
-                        -t|--tilde) tilde=1 ;;
-                        -T|--nameddirs)
-                                tilde=1
-                                named=1
-                        ;;
-                esac
-                shift
-        done
+  if zstyle -t ':prompt:shrink_path' fish; then
+    lastfull=1
+    short=1
+    tilde=1
+  fi
+  if zstyle -t ':prompt:shrink_path' nameddirs; then
+    tilde=1
+    named=1
+  fi
+  zstyle -t ':prompt:shrink_path' last && lastfull=1
+  zstyle -t ':prompt:shrink_path' short && short=1
+  zstyle -t ':prompt:shrink_path' tilde && tilde=1
 
-        typeset -a tree expn
-        typeset result part dir=${1-$PWD}
-        typeset -i i
+  while [[ $1 == -* ]]; do
+    case $1 in
+      -f|--fish)
+        lastfull=1
+        short=1
+        tilde=1
+        ;;
+      -h|--help)
+        print 'Usage: shrink_path [-f -l -s -t] [directory]'
+        print ' -f, --fish      fish-simulation, like -l -s -t'
+        print ' -l, --last      Print the last directory''s full name'
+        print ' -s, --short     Truncate directory names to the first character'
+        print ' -t, --tilde     Substitute ~ for the home directory'
+        print ' -T, --nameddirs Substitute named directories as well'
+        print 'The long options can also be set via zstyle, like'
+        print '  zstyle :prompt:shrink_path fish yes'
+        return 0
+        ;;
+      -l|--last) lastfull=1 ;;
+      -s|--short) short=1 ;;
+      -t|--tilde) tilde=1 ;;
+      -T|--nameddirs)
+        tilde=1
+        named=1
+        ;;
+    esac
+    shift
+  done
 
-        [[ -d $dir ]] || return 0
+  typeset -a tree expn
+  typeset result part dir=${1-$PWD}
+  typeset -i i
 
-        if (( named )) {
-                for part in ${(k)nameddirs}; {
-                        [[ $dir == ${nameddirs[$part]}(/*|) ]] && dir=${dir/#${nameddirs[$part]}/\~$part}
-                }
-        }
-        (( tilde )) && dir=${dir/#$HOME/\~}
-        tree=(${(s:/:)dir})
-        (
-                if [[ $tree[1] == \~* ]] {
-                        cd -q ${~tree[1]}
-                        result=$tree[1]
-                        shift tree
-                } else {
-                        cd -q /
-                }
-                for dir in $tree; {
-                        if (( lastfull && $#tree == 1 )) {
-                                result+="/$tree"
-                                break
-                        }
-                        expn=(a b)
-                        part=''
-                        i=0
-                        until [[ (( ${#expn} == 1 )) || $dir = $expn || $i -gt 99 ]]  do
-                                (( i++ ))
-                                part+=$dir[$i]
-                                expn=($(echo ${part}*(-/)))
-                                (( short )) && break
-                        done
-                        result+="/$part"
-                        cd -q $dir
-                        shift tree
-                }
-                echo ${result:-/}
-        )
+  [[ -d $dir ]] || return 0
+
+  if (( named )) {
+    for part in ${(k)nameddirs}; {
+      [[ $dir == ${nameddirs[$part]}(/*|) ]] && dir=${dir/#${nameddirs[$part]}/\~$part}
+    }
+  }
+
+  (( tilde )) && dir=${dir/#$HOME/\~}
+  tree=(${(s:/:)dir})
+  (
+
+  if [[ $tree[1] == \~* ]] {
+    cd -q ${~tree[1]}
+    result=$tree[1]
+    shift tree
+  } else {
+    cd -q /
+  }
+
+  for dir in $tree; {
+    if (( lastfull && $#tree == 1 )) {
+      result+="/$tree"
+      break
+    }
+    expn=(a b)
+    part=''
+    i=0
+    until [[ (( ${#expn} == 1 )) || $dir = $expn || $i -gt 99 ]]  do
+      (( i++ ))
+      part+=$dir[$i]
+      expn=($(echo ${part}*(-/)))
+      (( short )) && break
+    done
+    result+="/$part"
+    cd -q $dir
+    shift tree
+  }
+  echo ${result:-/}
+  )
 }
 
 # turns seconds into human readable time
@@ -204,7 +209,7 @@ prompt_pure_preprompt_render() {
 
 	# Set the path.
 	#preprompt_parts+=('%F{blue}%~%f')
-  preprompt_parts+=('%F{blue}$(shrink_path -f)%f')
+  preprompt_parts+=('%F{blue}$(prompt_pure_shrink_path -f)%f')
 
 	# Set color for git branch/dirty status, change color if dirty checking has
 	# been delayed.
